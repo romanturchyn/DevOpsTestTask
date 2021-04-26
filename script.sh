35087abc2788f42472ac41880a3cd2abebf7b84d
romt <romturchyn@gmail.com>
2021-04-26 16:47:18 +0300

    finished script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Written by Roman Turchyn 2021
 
+echo "Hello! This is a script for build docker images and automatically run Docker containers"
+echo "To date, we have two different choices - Ansible and Terraform containers"
 PS3='Please enter your choice: '
 options=("Docker Build and run Ansible container" "Docker Build and run Terraform container" "Show Docker images" "Quit")
 select opt in "${options[@]}"
@@ -7,16 +10,18 @@ do
     case $opt in
         "Docker Build and run Ansible container")
             echo "you chose Ansible"
-            docker build -t ansible-container ~/DevOpsTT/dockerfile-ansible
+            docker build -t ansible-docker ~/DevOpsTestTask/dockerfile-ansible && \
+            docker run -ti -v ~/DevOpsTestTask/dockerfile-ansible/ansible:/ansible --name ansible ansible-docker bash
 
             ;;
         "Docker Build and run Terraform container")
             echo "you chose Terraform"
-            docker build -t terraform-container ~/DevOpsTT/dockerfile-ansible
+            docker build -t terraform-docker ~/DevOpsTestTask/dockerfile-terraform && \
+            docker run -ti -v ~/DevOpsTestTask/dockerfile-terraform/terraform:/terraform --name terraform terraform bash
 
             ;;
         "Show Docker images")
-            echo "you chose Docker images"
+            echo "you chose Show Docker images"
             Docker images
             ;;
         "Quit")
